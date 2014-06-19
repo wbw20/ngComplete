9eeb271ca667e3eaa0532fb2e329cf5d73d52194
William B. Wettersten <wbw20@case.edu>
2014-06-18 23:13:45 -0700

    insert at cursor pos

--- src/ngComplete.js
+++ src/ngComplete.js
@@ -90,7 +90,11 @@ angular.module('ngComplete', [])
         }
 
         function _enter() {
-          $(element).val($(element).val() + results[_selected().index()].title);
+          var current = $(element).val(),
+              cursor = $(element).getCursorPosition(),
+              value = results[_selected().index()].title;
+
+          $(element).val([current.slice(0, cursor), value, current.slice(cursor)].join(''));
           _hide();
         }
 
@@ -141,3 +145,25 @@ angular.module('ngComplete', [])
       }
     };
   });
+
+/*
+ *  http://stackoverflow.com/questions/2897155/get-cursor-position-in-characters-within-a-text-input-field
+ */
+
+(function($) {
+    $.fn.getCursorPosition = function() {
+        var input = this.get(0);
+        if (!input) return; // No (input) element found
+        if ('selectionStart' in input) {
+            // Standard-compliant browsers
+            return input.selectionStart;
+        } else if (document.selection) {
+            // IE
+            input.focus();
+            var sel = document.selection.createRange();
+            var selLen = document.selection.createRange().text.length;
+            sel.moveStart('character', -input.value.length);
+            return sel.text.length - selLen;
+        }
+    }
+})(jQuery);
